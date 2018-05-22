cec217e5f109b3d154425d0bf21e65d1984ba25e
Kirimm <kaname97@mail.ru>
2018-05-22 15:28:21 +0300

    FirstVersion

--- StreamEditor/StreamEditor/Form1.Designer.cs
+++ StreamEditor/StreamEditor/Form1.Designer.cs
@@ -28,13 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выбратьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(310, 407);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem,
+            this.выбратьВсеToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.открытьToolStripMenuItem.Text = "открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem1_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.сохранитьToolStripMenuItem.Text = "сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.удалитьToolStripMenuItem.Text = "удалить";
+            this.удалитьToolStripMenuItem.Click += new System.EventHandler(this.удалитьToolStripMenuItem_Click);
+            // 
+            // выбратьВсеToolStripMenuItem
+            // 
+            this.выбратьВсеToolStripMenuItem.Name = "выбратьВсеToolStripMenuItem";
+            this.выбратьВсеToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.выбратьВсеToolStripMenuItem.Text = "выбрать все";
+            this.выбратьВсеToolStripMenuItem.Click += new System.EventHandler(this.выбратьВсеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выбратьВсеToolStripMenuItem;
     }
 }
 
--- StreamEditor/StreamEditor/Form1.cs
+++ StreamEditor/StreamEditor/Form1.cs
@@ -12,9 +12,161 @@ namespace StreamEditor
 {
     public partial class Form1 : Form
     {
+
+        protected string filepath;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public string Filepath
+        {
+            get { return filepath; }
+            set { filepath = value; }
+        }
+
+
+        private void создатьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+            open command = new open(richTextBox1);
+            MyMenu menu = new MyMenu(command);
+            menu.Ex();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save command = new save(richTextBox1);
+            MyMenu menu = new MyMenu(command);
+            menu.Ex();
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            delete command = new delete(richTextBox1);
+            MyMenu menu = new MyMenu(command);
+            menu.Ex();
+        }
+
+        private void выбратьВсеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll command = new SelectAll(richTextBox1);
+            MyMenu menu = new MyMenu(command);
+            menu.Ex();
+        }
+    }
+    interface ICommand
+    {
+        void Execute();
+    }
+    class MyMenu : MenuStrip
+    {
+        private ICommand command;
+
+        public MyMenu(ICommand cmd)
+        {
+            command = cmd;
+        }
+
+        public void Ex()
+        {
+            command.Execute();
+        }
+
+    }
+
+    class open : Form1, ICommand
+    {
+        RichTextBox r;
+
+        public open(RichTextBox rr)
+        {
+            r = rr;
+        }
+        public void Execute()
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Filepath = ofd.FileName;
+                try
+                {
+                    MessageBox.Show(Filepath);
+                    r.LoadFile(Filepath);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Поймано исключение: " + e + ".");
+                    Filepath = null;
+                }
+            }
+
+
+        }
+    }
+    class save : Form1, ICommand
+    {
+        RichTextBox r;
+
+        public save(RichTextBox rr)
+        {
+            r = rr;
+        }
+        public void Execute()
+        {
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                filepath = sfd.FileName;
+                try
+                {
+                    r.SaveFile(filepath);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Поймано исключение: " + e + ".");
+                    throw;
+                }
+            }
+
+
+
+        }
+
+    }
+    class SelectAll : Form1, ICommand
+    {
+        RichTextBox r;
+
+        public SelectAll(RichTextBox rr)
+        {
+            r = rr;
+        }
+        public void Execute()
+        {
+            r.SelectAll();
+
+        }
+
+    }
+
+
+    class delete : Form1, ICommand
+    {
+        RichTextBox r;
+
+        public delete(RichTextBox rr)
+        {
+            r = rr;
+        }
+
+        public void Execute()
+        {
+            r.SelectedText = "";
+        }
+
     }
 }
